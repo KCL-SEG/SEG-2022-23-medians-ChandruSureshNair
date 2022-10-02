658f0ca36e6f93471ec4a30064f85f0038e0dea1
Chandru Suresh <k21012031@kcl.ac.uk>
2022-10-02 20:24:55 +0100

    Implement median calculation and output

--- median.py
+++ median.py
@@ -8,5 +8,10 @@ while True:
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
-        break
-print(numbers)
+        n = len(numbers)
+        if n % 2:
+            median = numbers[n//2]
+            print(median)
+        else:
+            median = (numbers[n//2 - 1] + numbers[n//2]) / 2.0
+            print(median)
