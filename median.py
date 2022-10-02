c2dec5a4061d35da3e61706ea4a165a525dab9c7
Chandru Suresh <k21012031@kcl.ac.uk>
2022-10-02 20:44:27 +0100

    Add missing flush arguments

--- median.py
+++ median.py
@@ -3,10 +3,10 @@
 
 while True:
     try:
-        print("Enter a list of numbers separated by commas: ")
+        print("Enter a list of numbers separated by commas: ", flush=True)
         numbers = sorted([float(value) for value in input().split(",")])
     except ValueError:
-        print("Some input could not be converted to a number!")
+        print("Some input could not be converted to a number!", flush=True)
     except EOFError:
         exit()
     else:
